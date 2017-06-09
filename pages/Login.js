da2d7b7f4ca82c0381f78f393f1d9aaacdf9ada0
Vitaly Kushnir <v@codemotion.eu>
2017-06-09 17:20:15 +0300

    remove ignoreSync

--- pages/Login.js
+++ pages/Login.js
@@ -2,8 +2,6 @@
 
 import Base from '../utils/Base';
 
-browser.ignoreSynchronization = false;
-
 class LoginPage extends Base {
   get url() { return `${this.baseUrl}/login`; }
   get selector() { return element(by.css('span.logo-text')); }
