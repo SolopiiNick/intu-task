8007b3f8fc2fc887dc6222b7c06d7888ed1e4cc0
Vitaly Kushnir <v@codemotion.eu>
2017-06-14 15:49:16 +0300

    Assign tests to specs

--- configs/protractor-config.js
+++ configs/protractor-config.js
@@ -13,7 +13,7 @@ exports.config = {
   // webdriver-manager/selenium/selenium-server-standalone-3.3.1.jar",
   directConnect: true,
 
-  specs: ['../specs/processTransactionCheck.spec.js'],
+  specs: ['../specs/*.spec.js'],
   baseUrl: config.baseUrl,
   // framework: 'jasmine2',
 
