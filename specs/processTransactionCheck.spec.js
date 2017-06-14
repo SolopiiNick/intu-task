4877f00ec921440449bcf8f2a18ea1941fcc2309
Vitaly Kushnir <v@codemotion.eu>
2017-06-14 15:25:25 +0300

    Assign tests to specs

--- specs/processTransactionCheck.spec.js
+++ specs/processTransactionCheck.spec.js
@@ -1,9 +1,14 @@
 import { ProcessTransactionCheckLogic } from '../specsLogic';
 const checkLogic = new ProcessTransactionCheckLogic();
 
-describe('Process Transactions Check', () => {
+describe('Process Transactions - Check', () => {
   beforeAll(checkLogic.beforeAll.bind(checkLogic));
   beforeEach(checkLogic.beforeEach.bind(checkLogic));
 
-  it('shoud be visible', checkLogic.shouldBeVisible.bind(checkLogic));
+  it('Should be visible', checkLogic.shouldBeVisible.bind(checkLogic));
+  it('Send Saving With TEL', checkLogic.sendSavingWithTEL.bind(checkLogic));
+  it('Send Checking With PPD', checkLogic.sendCheckingWithPPD.bind(checkLogic));
+  it('Send Duplicate Saving With PPD', checkLogic.sendDuplicateSavingWithCCD.bind(checkLogic));
+  it('Send Billing Checking With PPD', checkLogic.sendBillingCheckingWithPPD.bind(checkLogic));
+  it('Send Refund Saving With PPD', checkLogic.sendRefundSavingWithCCD.bind(checkLogic));
 });
